88145cc8f045a9e79e7a29d3b1865d4b308175d2
Stoyan <Stoyan.botusharov@gmail.com>
2018-05-28 12:03:17 +0200

    dfsadsffdasfasedsf

---
+++ SL13/public_html/list.js
@@ -0,0 +1,15 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+var data = JSON.parse('./data.json');
+var list = new List();
+
+function getCategory(category cat){
+	for(var i=0;i<data;i++){
+		if(cat=data[i])
+
+	}
+}
